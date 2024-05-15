c4d5721b7f272f8ec11a090801932360adb7e6b5
Dhananjay Sahoo <kanha94377@gmail.com>
2024-05-15 17:26:50 +0530

    Initialize repository for Interncify

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+home=`<div class="container"> 
+<div class="d-flex p-3 flex-column justify-content-center align-items-center text-center " style="height: 100vh;">
+    <div class="heroheader mb-3 ">
+        <h1 >Unlock Your Potential with <span class='text-uppercase text-primary'> Interncify </span></h1>
+    </div>
+    <span class="mb-3">Empowering Future Tech Leaders</span>
+    <div class="heroheaderliks">
+        <a href="#" class="btn btn-primary me-4">Explore Internships</a>
+        <a href="#" class="text-decoration-none">Learn More &nbsp; <i class="fa fa-long-arrow-right" ></i></a>
+    </div>
+</div>
+</div>
+<div class="area" >
+<ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+</ul>
+</div >`;
+document.getElementById('home').innerHTML=home;
+
+
---
+++ scripts/nav.js
@@ -0,0 +1,34 @@
+nav=`<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container">
+  <a class="navbar-brand" href="#"><img height="40px" src="../assets/interncify_logo_1.png" alt=""></a>
+
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#feature">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#internships">Internships</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#ourteam">Our Team</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="contactus">Contact us</a>
+      </li>
+    </ul>
+    <div>
+        <a href="" class="btn fw-bold">Login</a>
+        <a href="" class="btn btn-primary fw-bold">Register</a>
+    </div>
+    
+  </div>
+</div>
+</nav>`
+document.getElementById('navigation').innerHTML=nav;
