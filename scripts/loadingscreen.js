88421cc352c28b0976d4928a3e06ce9325e53322
Dhananjay Sahoo <kanha94377@gmail.com>
2024-05-18 17:25:23 +0530

    test updated loadind screen

---
+++ scripts/loadingscreen.js
@@ -0,0 +1,5 @@
+$(window).on("load",function(){
+    //$(".wrapper-loader").fadeOut("slow")
+    // document.querySelectorAll(".wrapper-loader").style.display = "none"
+    $(".wrapper-loader").css("display","none");
+});
