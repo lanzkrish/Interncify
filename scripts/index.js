aece515d8f42219ccd93253e7c70c7349649ee26
Dhananjay Sahoo <kanha94377@gmail.com>
2024-07-26 08:41:38 +0530

    update v1.6.3 minor bug fixing and changes in the links

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,11 @@
 home = `<div class="container"> 
 <div class="d-flex p-3 flex-column justify-content-center align-items-center text-center " style="height: 100vh;">
     <div class="heroheader mb-3 ">
-        <h1 >Unlock Your Potential with <span class='text-uppercase text-primary'> Interncify </span></h1>
+        <h1 >Unlock Your Potential with <span class='text-lower text-primary ' > Interncify </span></h1>
     </div>
     <span class="mb-3">Empowering Future Tech Leaders</span>
     <div class="heroheaderliks">
-        <a href="https://forms.gle/ufnkk7KLsMSxkQZc6" target=_blank class="btn btn-primary me-4">Explore Internships</a>
+        <a href="./courses.html" target=_blank class="btn btn-primary me-4">Explore Internships</a>
         <a href="https://forms.gle/ufnkk7KLsMSxkQZc6" target=_blank class="text-decoration-none">Learn More &nbsp; <i class="fa fa-long-arrow-right" ></i></a>
     </div>
 </div>
