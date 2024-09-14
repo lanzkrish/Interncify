d82373c383d2cdb9aee222215fc48c1cfcd15636
Dhananjay Sahoo <kanha94377@gmail.com>
2024-09-14 19:04:04 +0530

    v2.1.8 courses page nav logo button link redirect to home

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,6 +1,6 @@
 nav=`<nav class="navbar navbar-expand-lg bg-white">
 <div class="container">
-  <a class="navbar-brand" href="#"><img height="40px" src="./assets/public/logo-interncify.png" alt=""></a>
+  <a class="navbar-brand" href="./"><img height="40px" src="./assets/public/logo-interncify.png" alt=""></a>
 
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
