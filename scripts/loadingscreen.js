15963388713cd0b8710e07bbce0c97b658dad3ea
Dhananjay Sahoo <kanha94377@gmail.com>
2024-09-27 09:18:56 +0530

    v2.1.10

--- scripts/loadingscreen.js
+++ scripts/loadingscreen.js
@@ -28,9 +28,10 @@ loadingstr=`<div class="wrapper-loader">
 </div>`
 
 document.getElementById('loader').innerHTML=loadingstr;
-
+// document.querySelectorAll("scroll").style.overflow ="none"
 $(window).on("load",function(){
-    //$(".wrapper-loader").fadeOut("slow")
-    // document.querySelectorAll(".wrapper-loader").style.display = "none"
-    $(".wrapper-loader").css("display","none");
+    $(".wrapper-loader").fadeOut("slow")
+    document.querySelectorAll(".wrapper-loader").style.display = "none"
+    // document.body.classList.remove('scroll');
+    // $(".wrapper-loader").css("display","none");
 });
